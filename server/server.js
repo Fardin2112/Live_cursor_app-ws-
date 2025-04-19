9d2fe9b42c2373174bf863779ba755c810b9b7c4
Fardin2112 <fardinalam2112@gmail.com>
2025-04-19 09:41:17 +0530

    server side works complete

--- server/server.js
+++ server/server.js
@@ -8,6 +8,34 @@ const wsServer = new WebSocketServer({ server})
 
 const port = 8000
 
+const users = { }
+const connections = {}
+
+const broadcast = () => {
+    Object.keys(connections).forEach(uuid => {
+        const connection = connections[uuid];
+        const message = JSON.stringify(users)
+        connection.send(message);
+    })
+}
+
+const handleMessage = (bytes , userId) => {
+    const message = JSON.parse(bytes.toString());
+    const user = users[userId];
+    user.state = message;
+
+    broadcast();
+
+    console.log(`$${user.username} update their state : ${JSON.stringify(user.state)}`);
+}
+
+const handleClose = userId => {
+    
+    console.log(`${users[userId].username} disconnected`)
+    delete connections[userId];
+    delete users[userId];
+}
+
 wsServer.on("connection", (connection, request) => {
     // ws : // http:localhost
     // wss : // https
@@ -16,7 +44,17 @@ wsServer.on("connection", (connection, request) => {
     const userId = uuidv4();
     console.log(username)
     console.log(userId);
+
+    connections[userId] = connection
     
+    users[userId] = {
+        username,
+        state : {}
+    }
+
+    connection.on("message", message => handleMessage(message,userId));
+    connection.on("close",()=> handleClose(userId))
+
 })
 
 server.listen(port, ()=>{
